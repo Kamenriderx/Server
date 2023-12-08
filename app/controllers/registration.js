a043b77a79177bf7f3a9b8c2307044c46381f89b
Kamenriderx <kiritojeiger@gmail.com>
2023-12-08 14:46:37 -0600

    Registro

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -29,13 +29,13 @@ async function login(req, res) {
 }
 
 async function addUser(req, res) {
-  const { action, codUser, codRol, userName, password } = req.body;
-  console.log(action, codUser, codRol, userName, password);
+  const { codRol,userName,password,email } = req.body;
+  console.log(codRol,userName,password,email);
   try {
     connection
       .query(
-        `CALL GESTION_USUARIOS(:action,:codUser,:codRol,:userName,:password,@PV_result)`,
-        { replacements: { action, codUser, codRol, userName, password } }
+        `INSERT INTO TBL_USUARIOS(COD_ROL,NOM_USUARIO,CONTRASENA,CORREO) VALUES(:codRol,:userName,:password,:email) `,
+        { replacements: { codRol,userName,password,email } }
       )
       .then((ress) => {
         console.log(ress);
@@ -46,7 +46,30 @@ async function addUser(req, res) {
   }
 }
 
+
+const getUser = async (req, res) => {
+  try{
+    let data = [];
+    connection.query(`SELECT * FROM TBL_USUARIOS WHERE NOM_USUARIO != 'admin'`).then(ress=>{
+      
+       data = ress[0].map(user=>{return{
+        cod_usuario:user.COD_USUARIO,
+        nom_usuario:user.NOM_USUARIO,
+        contrasena:user.CONTRASENA,
+        fec_registro:(user.FEC_REGISTRO),
+        email:user.CORREO
+      }});
+      res.status(200).json({data});
+    })
+  }catch(e){
+    res.status(400).json({message:"Error"});
+  }
+};
+
+
+
 module.exports = {
   login,
   addUser,
+  getUser
 };
--- app/routes/register.js
+++ app/routes/register.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router()
-const {login,addUser} = require("../controllers/registration");
+const {login,addUser,getUser} = require("../controllers/registration");
 
 
 const  authToken = require("../middlewares/authToken");
 
 router.post("/login",login);
 router.post("/addUser",authToken,addUser);
+router.get("/getUsers",authToken,getUser);
 
 
 module.exports = router
